f551b9dccdf4197550ffa128c11a24141591cf16
James Yap <contact@jamesyap.org>
2025-10-11 12:31:21 -0400

    chore: prior to Vite

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === 'install') {
-    chrome.storage.local.set({
-      apiSuggestions: ['tabs', 'storage', 'scripting']
-    });
+    // chrome.storage.local.set({
+    //   apiSuggestions: ['tabs', 'storage', 'scripting']
+    // });
   }
 });
 
 // Listen for messages from content scripts
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  
+  // Scotia
   const apiPattern = /transaction-history/;
   if (message.type === 'RESPONSE_BODY' && apiPattern.test(message.url)) {
     console.log('Background received:', message.url, JSON.parse(message.body));
