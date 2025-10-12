d967adebd09223b497be481054ab7a01e52a58a0
James Yap <contact@jamesyap.org>
2025-10-12 14:11:35 -0400

    chore: added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   
   // Use relative paths for Chrome extension
   base: './',
