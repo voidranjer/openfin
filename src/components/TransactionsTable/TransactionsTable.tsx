f808abfcf98c830edbe37a32692dbd491583574a
James Yap <contact@jamesyap.org>
2025-11-06 03:50:20 -0500

    style: initial col widths

--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -5,55 +5,86 @@ import { formatDate } from "@/chrome/core/utils";
 
 type Props = {
   transactions: FireflyTransaction[];
-}
+};
 
 export default function TransactionsTable({ transactions }: Props) {
-
-  const descriptionCellRenderer = (rowIndex: number) => <Cell>{transactions[rowIndex].description}</Cell>;
-  const dateCellRenderer = (rowIndex: number) => <Cell style={{ textAlign: "center" }}>{formatDate(transactions[rowIndex].date)}</Cell>;
-  const categoryCellRenderer = (rowIndex: number) => <Cell style={{ textAlign: "center" }}>{transactions[rowIndex].category_name}</Cell>;
+  const descriptionCellRenderer = (rowIndex: number) => (
+    <Cell>{transactions[rowIndex].description}</Cell>
+  );
+  const dateCellRenderer = (rowIndex: number) => (
+    <Cell style={{ textAlign: "center" }}>
+      {formatDate(transactions[rowIndex].date)}
+    </Cell>
+  );
+  const categoryCellRenderer = (rowIndex: number) => (
+    <Cell style={{ textAlign: "center" }}>
+      {transactions[rowIndex].category_name}
+    </Cell>
+  );
   const amountCellRenderer = (rowIndex: number) => {
     const transaction = transactions[rowIndex];
-    return transaction.type === "deposit" ? <Cell style={{ color: "green", textAlign: "end" }}>{transaction.amount.toFixed(2)}</Cell>
-      : <Cell style={{ color: "red", textAlign: "end" }}>{transaction.amount.toFixed(2)}</Cell>;
-  }
+    return transaction.type === "deposit" ? (
+      <Cell style={{ color: "green", textAlign: "end" }}>
+        {transaction.amount.toFixed(2)}
+      </Cell>
+    ) : (
+      <Cell style={{ color: "red", textAlign: "end" }}>
+        {transaction.amount.toFixed(2)}
+      </Cell>
+    );
+  };
   // const externalIdCellRenderer = (rowIndex: number) => <Cell>{transactions[rowIndex].external_id}</Cell>;
-  const notesCellRenderer = (rowIndex: number) => <Cell>{transactions[rowIndex].notes ?? ""}</Cell>;
+  const notesCellRenderer = (rowIndex: number) => (
+    <Cell>{transactions[rowIndex].notes ?? ""}</Cell>
+  );
 
   const columns = [
     {
-      name: "Description",
-      cellRenderer: descriptionCellRenderer
+      name: "Date",
+      cellRenderer: dateCellRenderer,
     },
     {
-      name: "Date",
-      cellRenderer: dateCellRenderer
+      name: "Description",
+      cellRenderer: descriptionCellRenderer,
     },
     {
       name: "Category",
-      cellRenderer: categoryCellRenderer
+      cellRenderer: categoryCellRenderer,
     },
     {
       name: "Amount",
-      cellRenderer: amountCellRenderer
+      cellRenderer: amountCellRenderer,
     },
     {
       name: "Notes",
-      cellRenderer: notesCellRenderer
-    }
-  ]
+      cellRenderer: notesCellRenderer,
+    },
+  ];
 
+  const columnWidths = [100, 300, 150, 100, 300];
 
   const headerRowRenderer = (colIdx: number) => (
-    <ColumnHeaderCell className="text-center font-bold" >{columns[colIdx].name}</ColumnHeaderCell>
+    <ColumnHeaderCell className="text-center font-bold">
+      {columns[colIdx].name}
+    </ColumnHeaderCell>
   );
 
-
-  return (<>
-
-    <Table numRows={transactions.length} cellRendererDependencies={transactions}>
-      {columns.map((col, idx) => (
-        <Column key={idx} name={col.name} cellRenderer={col.cellRenderer} columnHeaderCellRenderer={headerRowRenderer} />
-      ))}
-    </Table> </>)
+  return (
+    <>
+      <Table
+        numRows={transactions.length}
+        cellRendererDependencies={transactions}
+        columnWidths={columnWidths}
+      >
+        {columns.map((col, idx) => (
+          <Column
+            key={idx}
+            name={col.name}
+            cellRenderer={col.cellRenderer}
+            columnHeaderCellRenderer={headerRowRenderer}
+          />
+        ))}
+      </Table>{" "}
+    </>
+  );
 }
